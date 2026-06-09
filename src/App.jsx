f096f51dc840d22f56356412903cfb13587e5256
hyeon <ehdgusdl8362@gmail.com>
2026-06-09 22:38:38 +0900

    품목 추가 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { supabase } from "./services/supabase";
 function App() {
 
   const [items, setItems] = useState([]);
+  const [itemCode, setItemCode] = useState("");
+  const [itemName, setItemName] = useState("");
+  const [drawingNo, setDrawingNo] = useState("");
 
   useEffect(() => {
     loadItems();
@@ -23,10 +26,58 @@ function App() {
     setItems(data);
   }
 
+  async function addItem() {
+
+    const { error } = await supabase
+      .from("items")
+      .insert([
+        {
+          item_code: itemCode,
+          item_name: itemName,
+          drawing_no: drawingNo
+        }
+      ]);
+  
+    if (error) {
+      console.error(error);
+      return;
+    }
+  
+    loadItems();
+  
+    setItemCode("");
+    setItemName("");
+    setDrawingNo("");
+  }
+
   return (
     <div style={{ padding: "20px" }}>
 
       <h1>품목관리</h1>
+
+      <div>
+      <input
+        placeholder="품목코드"
+        value={itemCode}
+        onChange={(e) => setItemCode(e.target.value)}
+      />
+
+      <input
+        placeholder="품목명"
+        value={itemName}
+        onChange={(e) => setItemName(e.target.value)}
+      />
+
+      <input
+        placeholder="도면번호"
+        value={drawingNo}
+        onChange={(e) => setDrawingNo(e.target.value)}
+      />
+
+      <button onClick={addItem}>
+        품목 추가
+      </button>
+    </div>
 
       <table border="1">
         <thead>
